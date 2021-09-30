8cdcc8a14341f45dca36cb6ea2c04ba063a1818d
kkhanhluu <luukhanh96@gmail.com>
2021-09-30 11:34:07 +0200

    refactor: create singleton easybill

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,24 @@ import { CustomerAPI } from './customer/api';
 import { DocumentAPI } from './document/api';
 
 export class EasybillClient {
+  private static instance: EasybillClient;
+
   public readonly customerAPI: CustomerAPI;
 
   public readonly documentAPI: DocumentAPI;
 
-  constructor(apiKey: string) {
+  private constructor(apiKey: string) {
     const baseURL = 'https://api.easybill.de/rest/v1';
 
     this.customerAPI = new CustomerAPI(baseURL, apiKey);
     this.documentAPI = new DocumentAPI(baseURL, apiKey);
+  }
+
+  public static getInstance(apiKey: string): EasybillClient {
+    if (!EasybillClient.instance) {
+      EasybillClient.instance = new EasybillClient(apiKey);
+    }
+    return EasybillClient.instance;
   }
 }
 
